bd7d2c0db416663ece858027e51140a810ccaafd
Tiago Gomes <ghomes.thyago@gmail.com>
2020-04-20 14:37:36 -0300

    First commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,84 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repositorie);
+
+  return response.status(200).json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repositorie ID is not valid!!" });
+  };
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found!'});
+  }
+
+  const likes = repositories[repositorieIndex].likes;
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.status(200).json(repositories[repositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repositorie ID is not valid!!" });
+  };
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found!'});
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Repositorie ID is not valid!!" });
+  };
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found!'});
+  }
+
+  const likes = repositories[repositorieIndex].likes + 1;
+
+  repositories[repositorieIndex].likes = likes;
+
+  return response.status(200).json(repositories[repositorieIndex]);
+
 });
 
 module.exports = app;
